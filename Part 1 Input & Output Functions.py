d4c8320b5da0108af885c225d406cfd8120ab3f7
SAMJ-99 <abdulmutlib786786@gmail.com>
2026-03-04 20:26:57 +0500

    Update Part 1 Input & Output Functions.py

--- Part 1 Input & Output Functions.py
+++ Part 1 Input & Output Functions.py
@@ -1,3 +1,10 @@
-#First task
+#First Task
 print ("Hello, World")
 print (Abdul)
+
+#Second Task
+User_FavColor = input("What is your favorite color?")
+print (f"Your favorite color is {User_FavColor}")
+
+#Third Task
+print ("Nice","color","btw",sep="-")
