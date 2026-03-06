de503f5a337c3372b088c31a97219f1090d78ff9
SAMJ-99 <abdulmutlib786786@gmail.com>
2026-03-06 19:29:37 +0500

    My Code

--- Part 1 Input & Output Functions.py
+++ Part 1 Input & Output Functions.py
@@ -1,10 +1,12 @@
-#First Task
+#1.
 print ("Hello, World")
 print ("Abdul")
 
-#Second Task
+#2.Ask the user for their favorite color using input() and print "Your favorite color is [color]"
 User_FavColor = input("What is your favorite color?")
 print (f"Your favorite color is {User_FavColor}")
 
-#Third Task
+#3.Use a single print() statement to display three different words separated by a hyphen
 print ("Nice","color","btw",sep="-")
+
+#4.Prompt the user for their birth year and print their age
